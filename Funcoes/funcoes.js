fcf9b309942b8929a4e0802bf70419b1b0c32b06
Giovana Rocha <100931504+giovanarochaz@users.noreply.github.com>
2023-08-01 12:44:14 -0300

    Aula 01/08/2023

---
+++ Funcoes/funcoes.js
@@ -0,0 +1,15 @@
+saldo = 1000
+
+function dadosacesso(){
+    numcartao = prompt('Digite o número do seu cartão: ')
+    senha = prompt('Digite a senha do seu cartão: ')
+}
+
+function sacar(valor){
+
+    saldo = saldo - valor
+    return saldo
+
+}
+
+
