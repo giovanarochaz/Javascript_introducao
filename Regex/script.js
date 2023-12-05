6079cc10ee57ff9128b9565fb66e8abb94dfdd06
Giovana Rocha <100931504+giovanarochaz@users.noreply.github.com>
2023-12-05 12:15:37 -0300

    Add files via upload

--- Regex/script.js
+++ Regex/script.js
@@ -45,3 +45,50 @@ function destacarPalavrasANO() {
     
     document.getElementById('poemOutput').innerHTML = output;
 }
+
+// Seleciona o elemento com id main-image (imagem que vai ficar no topo)
+const mainImage = document.getElementById("main-image");
+
+// Seleciona todos os elementos que tem a classe "bottom-image"
+const bottomImages = document.querySelectorAll(".bottom-image");
+
+// Roda um lalo de repetição pata cada elemento do bottomImages
+bottomImages.forEach((image, index) => {
+    // Escuta o clique encima de um dos elementos, com o objeto image
+  image.addEventListener("click", () => {
+    // Obtém o caminho da imagem atual na linha superior
+    const currentImageSrc = mainImage.getAttribute("src");
+    // Define o caminho da imagem na linha superior como o caminho da imagem clicada na linha inferior
+    mainImage.setAttribute("src", image.getAttribute("src"));
+    //// Define o caminho da imagem clicada na linha inferior como o caminho da imagem anterior da linha superior
+    image.setAttribute("src", currentImageSrc);
+  });
+});
+
+function enviarInput() {
+    var valor = document.getElementById("inputCampo").value;
+    document.getElementById("resposta").textContent = valor;
+};
+
+$(document).ready(function () {
+    // Máscara para o campo de telefone
+    $('#phone').inputmask('(99) 99999-9999', { placeholder: '' });
+
+    // Validação do formulário
+    $('#myForm').submit(function (event) {
+      // Validar o telefone usando regex
+      var phoneRegex = /^\(\d{2}\) \d{5}-\d{4}$/;
+      var phoneValue = $('#phone').val();
+
+      if (!phoneRegex.test(phoneValue)) {
+        alert('Por favor, insira um número de telefone válido.');
+        event.preventDefault();
+        return false;
+      }
+
+      // Adicione outras validações conforme necessário
+
+      // Se todas as validações passarem, o formulário será enviado
+      alert('Formulário enviado com sucesso!');
+    });
+  });
